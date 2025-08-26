2bcfc14dddcce95acbc3470c5349a8dce3bbb3ff
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-08-26 19:17:55 -0300

    language switcher

--- src/app/components/menu/menu.ts
+++ src/app/components/menu/menu.ts
@@ -2,10 +2,12 @@ import { Component, output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatMenuModule } from '@angular/material/menu';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-menu',
-  imports: [MatIconModule, MatToolbarModule, MatButtonModule],
+  imports: [MatIconModule, MatToolbarModule, MatButtonModule, MatMenuModule],
   templateUrl: './menu.html',
   styleUrl: './menu.scss'
 })
@@ -13,6 +15,21 @@ export class Menu {
   navToggle = output<boolean>();
   handleClick() {
     this.navToggle.emit(true);
+  }
+  languages = [
+    { "shortForm": "en", "fullForm": "English" },
+    { "shortForm": "pt-BR", "fullForm": "Português" }];
+  defaultLanguage = "en";
+  currentLanguage = this.defaultLanguage;
+  constructor(private translate: TranslateService) {
+    this.translate.addLangs(this.languages.map(l => l.shortForm));
+    this.translate.setFallbackLang(this.defaultLanguage);
+    this.translate.use(this.defaultLanguage);
+  }
+
+  switchLanguage(language: string) {
+    this.translate.use(language);
+    this.currentLanguage = language;
   }
 
 }
