62e6eb2a076b479943ef00601dc4a9be01a1e975
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-09-05 22:20:14 -0300

    not found page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { About } from './pages/about/about';
+import { NotFound } from './pages/not-found/not-found';
 
 export const routes: Routes = [
   {
     path: 'about',
     component: About
+  },
+  {
+    path: '**',
+    component: NotFound
   }  
 ];
