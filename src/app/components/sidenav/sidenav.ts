2d9e1660b8cbb1c3a455ec62991aa94d66b9b7f9
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-09-09 19:58:45 -0300

    creating links between pages

--- src/app/components/sidenav/sidenav.ts
+++ src/app/components/sidenav/sidenav.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { Menu } from "../menu/menu";
 import { MatSidenavModule } from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [Menu, MatSidenavModule, MatListModule],
+  imports: [Menu, MatSidenavModule, MatListModule,
+    RouterLink, RouterLinkActive, TranslateModule],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.scss'
 })
