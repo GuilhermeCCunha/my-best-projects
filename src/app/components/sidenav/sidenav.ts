90c61f90a8b4e14a78762354130a0ab079131038
GuilhermeCCunha <guilhermecdacunha@gmail.com>
2025-09-19 18:17:55 -0300

    sidenav with icons

--- src/app/components/sidenav/sidenav.ts
+++ src/app/components/sidenav/sidenav.ts
@@ -4,11 +4,12 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-sidenav',
   imports: [Menu, MatSidenavModule, MatListModule,
-    RouterLink, RouterLinkActive, TranslateModule],
+    RouterLink, RouterLinkActive, TranslateModule, MatIconModule],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.scss'
 })
